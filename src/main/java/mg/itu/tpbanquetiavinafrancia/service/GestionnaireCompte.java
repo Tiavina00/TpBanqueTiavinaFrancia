9d48a96f423dc7675a285eda9fbcc6b46645dc48
TiavinaFrancia42 <randrianarivofrancia@gmail.com>
2024-04-18 13:53:58 +0300

    Déclaration de la source de données et mis à jour du plugins maven

---
+++ src/main/java/mg/itu/tpbanquetiavinafrancia/service/GestionnaireCompte.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquetiavinafrancia.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *pour gérer les comptes bancaires dans la base de données.
+ * @author randr
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="root", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+@RequestScoped
+public class GestionnaireCompte {
+    
+}
